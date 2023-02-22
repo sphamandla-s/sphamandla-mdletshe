35c278d155569ad563f6a589e19a5d90a01b69bd
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2023-02-23 00:53:12 +0200

    still have to work on the project section

---
+++ pages/components/Education.js
@@ -0,0 +1,20 @@
+export const Education = ()=>{
+    return (
+        <div id="education" className='font-medium'>
+                <div className='pb-5'>
+                  <span className="">2020 - Present</span>
+                  <h3>
+                    <a href="#" className='text-rose-600 cursor-pointer font-semibold'>Diploma in Law </a>
+                    at University of South Africa
+                  </h3>
+                </div>
+                <div className='pb-5'>
+                  <span className="">2021 - 2022</span>
+                  <h3> <a href="#" className='text-rose-600 cursor-pointer font-semibold'>NQF level 5
+                    Information Technology: Systems Development </a>
+                    at WeThinkCode_
+                  </h3>
+                </div>
+              </div>
+    )
+}
---
+++ pages/components/Skills.js
@@ -0,0 +1,24 @@
+export const Skills = ()=>{
+    return (
+        <div id='skills'>
+                <ul className='grid grid-flow-col grid-rows-5 grid-cols-2 font-medium gap-1'>
+                  <li>Python</li>
+                  <li>Java(Object Oriented programming)</li>
+                  <li>Javascript</li>
+                  <li>HTML & CSS</li>
+                  <li>Dart & Flutter</li>
+                  <li>React</li>
+                  <li>Next js</li>
+                  <li>SQL</li>
+                  <li>Docker</li>
+                  <li>MangoDB</li>
+                  <li>Express</li>
+                  <li>Node js</li>
+                  <li>Tailwind</li>
+                  <li>Devops</li>
+                  <li>Git</li>
+                </ul>
+
+              </div>
+    )
+}
